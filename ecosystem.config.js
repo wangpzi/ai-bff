849534ac2243d5fa61b303bdba919abd81a5d49a
王胖子 <wpang520@163.com>
2025-06-20 21:40:25 +0800

    修改pm2配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,14 +8,14 @@ module.exports = {
       exec_mode: 'cluster',
       interpreter: './node_modules/.bin/ts-node', // 使用本地 ts-node
       autorestart: true,
-      watch: true,
+      watch: false,
       env: {
         NODE_ENV: 'development',
-        TS_NODE_PROJECT: './tsconfig.json',
+        TS_NODE_TRANSPILE_ONLY: 'true'
       },
       env_production: {
         NODE_ENV: 'production',
-        TS_NODE_PROJECT: './tsconfig.json',
+        TS_NODE_TRANSPILE_ONLY: 'true'
       },
       error_file: './logs/ai-bff-error.log',
       out_file: './logs/ai-bff-out.log',
